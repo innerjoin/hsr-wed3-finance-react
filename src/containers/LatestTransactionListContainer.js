cf7f054297c2c29dd573542333fadfa2bacf37be
Janic Mikes <janic.mikes@gmail.com>
2017-03-31 12:33:27 +0200

    Add import for api

--- src/containers/LatestTransactionListContainer.js
+++ src/containers/LatestTransactionListContainer.js
@@ -1,17 +1,40 @@
 // @flow
 
-import React, { Component} from 'react';
+import React, {Component} from 'react';
+import {getTransactions} from '../api'
+
 export type Props = {
     token: string,
+    fromDate: string,
+    toDate: string,
+    count: number,
+    skip: number,
 }
+
 
 class LatestTransactionListContainer extends React.Component {
 
     props: Props
 
-    render() {
+    // state = {
+    //     token: "test",
+    //     fromDate: "2017-01-01",
+    //     toDate: "2017-12-31",
+    //     count: "3",
+    //     skip: "0",
+    //     error: null
+    // }
 
+    render() {
+        // const {token, fromDate, toDate, count, skip} = this.state
+        // getTransactions(token, fromDate, toDate, count, skip).then(result => {
+        //     console.log("getTransaction result ", result)
+        //     //this.setState({redirectToReferrer: true, error: null})
+        // }).catch(error =>
+        //     this.setState({error})
+        // )
         return (
+            //getTransactions()
             <div>Latest Transaction List Container</div>
         )
     }
