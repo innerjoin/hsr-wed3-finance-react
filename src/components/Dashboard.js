d4a657e8fbb660b40d29c3cc969759cb461ca77d
Janic Mikes <janic.mikes@gmail.com>
2017-03-31 11:41:18 +0200

    add new payment and dashboard stuff

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,35 +1,49 @@
 // @flow
 
 import React from 'react'
+import {Grid, Segment} from 'semantic-ui-react'
+import NewPaymentContainer from '../containers/NewPaymentContainer'
+import LatestTransactionListContainer from '../containers/LatestTransactionListContainer'
 
 /*
-  Use the api functions to call the API server. For example, the transactions
-  can be retrieved and stored in the state as follows:
-  
-  getTransactions(this.props.token)
-    .then(({result: transactions}) => 
-      this.setState({transactions})
-    )
-    
-  import { getAccountDetails, getAccount, transfer, getTransactions } from '../api'
-*/
+ Use the api functions to call the API server. For example, the transactions
+ can be retrieved and stored in the state as follows:
+
+ getTransactions(this.props.token)
+ .then(({result: transactions}) =>
+ this.setState({transactions})
+ )
+
+ import { getAccountDetails, getAccount, transfer, getTransactions } from '../api'
+ */
 
 export type Props = {
-  token: string,
+    token: string,
 }
 
 class Dashboard extends React.Component {
-  
-  props: Props
-  
-  render() {    
 
-    return (
-      <div>
-        This is the dashboard
-      </div>
-    )
-  }
+    props: Props
+
+    render() {
+
+        return (
+            <Grid container columns={2}>
+                <Grid.Row>
+                    <Grid.Column width={6}>
+                        <Segment>
+                            <NewPaymentContainer />
+                        </Segment>
+                    </Grid.Column>
+                    <Grid.Column width={10}>
+                        <Segment>
+                            <LatestTransactionListContainer />
+                        </Segment>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        )
+    }
 }
 
 export default Dashboard
